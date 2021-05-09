491bc2f767d3f04af6ec79e20e735acda97a43b9
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-05-09 23:47:11 +0200

    Create condition if list is not empty if yes return nan

--- inferential_stat_lec7_exercise3.py
+++ inferential_stat_lec7_exercise3.py
@@ -11,3 +11,11 @@ def std_dev_of_lengths(L):
     returns: float, the standard deviation of the lengths of the strings,
       or NaN if L is empty.
     """
+    list_of_str = L
+    len_of_list = len(list_of_str)
+    if len_of_list == 0:
+        return float('NaN')
+    else:
+        return 0
+    
+print(std_dev_of_lengths([]))
