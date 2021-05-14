760e35c137edde7481c2723d41feb28f4b6cf666
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-05-14 20:25:56 +0200

    Add get_mean_and_std function

---
+++ central_limit_theorem_lec_8.py
@@ -0,0 +1,12 @@
+import random
+import pylab
+
+
+def get_mean_and_std(X):
+    mean = sum(X)/float(len(X))
+    tot = 0.0
+    for x in X:
+        tot += (x - mean)**2
+    std = (tot/len(X))**2
+    return mean, std
+
