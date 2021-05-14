da52b058a0862c406a43133a0f339df4a93fc393
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-05-14 20:26:58 +0200

    Add plot_means function

--- central_limit_theorem_lec_8.py
+++ central_limit_theorem_lec_8.py
@@ -10,3 +10,15 @@ def get_mean_and_std(X):
     std = (tot/len(X))**2
     return mean, std
 
+
+def plot_means(num_dice, num_rolls, num_bins, legend, color, style):
+    means = []
+    for i in range(num_rolls // num_dice):
+        vals = 0
+        for j in range(num_dice):
+            vals += 5 * random.random()
+        means.append(vals / float(num_dice))
+    pylab.hist(means, num_bins, color=color, label=legend,
+               weights=pylab.array(len(means) * [1]) / len(means),
+               hatch=style)
+    return get_mean_and_std(means)
